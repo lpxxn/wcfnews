33997cb8588795c8b187b4b00fa44453aa0e3311
lpxxn <lpxxn@foxmail.com>
2016-02-21 15:38:46 +0800

    test

---
+++ WcfNews/IImageOperate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfNews
+{
+    [ServiceContract(Namespace = "IImageOperate")]
+    public interface IImageOperate
+    {
+
+    }
+}
