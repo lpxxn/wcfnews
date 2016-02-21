7a4f3ee2a2f5991066969d2dd66be321e10a030a
lpxxn <lpxxn@foxmail.com>
2016-02-21 16:46:36 +0800

    t

---
+++ NewImplementation/NewsOperate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WcfNews;
+
+namespace NewImplementation
+{
+    class NewsOperate : INewsOperate
+    {
+        public bool UpLoadImage()
+        {            
+            return true;
+        }
+    }
+}
--- WcfNews/INewsOperate.cs
+++ WcfNews/INewsOperate.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WcfNews
 {
-    [ServiceContract(Namespace = "IImageOperate")]
-    public interface IImageOperate
+    [ServiceContract(Namespace = "INewsOperate")]
+    public interface INewsOperate
     {
+        bool UpLoadImage();
 
     }
 }
