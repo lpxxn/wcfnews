64cbaeb485cf01df7d53635580db656a5b0810b2
lpxxn <lpxxn@foxmail.com>
2016-03-06 15:24:04 +0800

    parse

---
+++ NewImplementation/OperateImage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewImplementation
+{
+    public class OperateImage
+    {
+        public void ParseImage(Stream stream)
+        {
+            if (!stream.CanRead)
+                return;
+        }
+    }
+}
+
+
+// http://stackoverflow.com/questions/9734941/upload-file-from-html-form-multipart-form-data-to-wcf-rest-service-as-a-stream
+//http://stackoverflow.com/questions/27759829/wcf-receive-stream-from-afnetworking/27772504#27772504
